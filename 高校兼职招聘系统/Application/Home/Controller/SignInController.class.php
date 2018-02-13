959df46692fce8ba26d6b23a07d5f13cf2ce45af
魏继阳 <845612246@qq.com>
2018-02-13 11:02:08 +0800

    Add files via upload

--- 高校兼职招聘系统/Application/Home/Controller/SignInController.class.php
+++ 高校兼职招聘系统/Application/Home/Controller/SignInController.class.php
@@ -1,9 +1,8 @@
 <?php
 namespace Home\Controller;
 use Think\Controller;
-class IndexController extends Controller {
+class SignInController extends Controller {
     public function index(){
-    	echo "这里是index controller ！";
         $this->show();
     }
 }
